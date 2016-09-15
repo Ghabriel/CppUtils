e12d242d32d540a0a5ea1a4df8570feb94f3e62c
Ghabriel Nunes <ghabriel.nunes@gmail.com>
2016-09-15 15:15:58 -0300

    Began the rework of type

--- src/type.cpp
+++ src/type.cpp
@@ -1,162 +1,99 @@
 /* created by Ghabriel Nunes <ghabriel.nunes@gmail.com> [2016] */
-#include <unordered_map>
+
+#include <cassert>
+#include "macros.hpp"
 #include "type.hpp"
 
-std::vector<utils::__detail::type_info> utils::__detail::typeStringToVector(const std::string& str) {
-    std::vector<__detail::type_info> result;
-    std::string buffer;
-    std::size_t size = str.size();
-    bool completeBuffer = false;
-    for (std::size_t i = 0; i < size; i++) {
-        char c = str[i];
-        if (c >= '0' && c <= '9') {
-            completeBuffer = true;
-            std::string temp;
-            while (str[i] >= '0' && str[i] <= '9') {
-                temp += str[i];
-                i++;
-            }
-            unsigned long long size = stoull(temp);
-            temp.clear();
-            for (unsigned long long j = 0; j < size; j++) {
-                temp += str[i];
-                i++;
-            }
-            i--;
-            buffer += std::to_string(temp.size()) + temp;
-        } else if (c != 'I') {
-            buffer += c;
-            if (c <= 'A' || c >= 'Z') {
-                completeBuffer = true;
-            }
-        } else {
-            completeBuffer = true;
-            std::size_t nestingLevel = 0;
-            while (i < size && (str[i] != 'E' || nestingLevel > 1)) {
-                if (str[i] == 'I') {
-                    nestingLevel++;
-                } else if (str[i] == 'E') {
-                    nestingLevel--;
-                }
-                buffer += str[i];
-                i++;
-            }
-            buffer += str[i];
-        }
+size_t utils::__detail::catchNumber(const std::string& content, size_t& index) {
+    std::string result;
+    while (content[index] >= '0' && content[index] <= '9') {
+        result += content[index];
+        index++;
+    }
+    return std::stoul(result);
+}
 
-        if (i == size - 1 || (str[i+1] != 'I' && completeBuffer)) {
-            result.push_back(typeFromString(buffer));
-            buffer.clear();
-            completeBuffer = false;
-        }
+std::string utils::__detail::capture(const std::string& content, size_t& index) {
+    auto size = catchNumber(content, index);
+    std::string result;
+    for (size_t i = 0; i < size; i++) {
+        result += content[index];
+        index++;
     }
     return result;
 }
 
-utils::__detail::type_info utils::__detail::typeFromString(const std::string& baseName) {
-    __detail::type_info result;
-    std::unordered_map<char, std::string> baseTypes = {
-        {'b', "bool"},
-        {'c', "char"},
-        {'d', "double"},
-        {'e', "long double"},
-        {'f', "float"},
-        {'h', "unsigned char"},
-        {'i', "int"},
-        {'j', "unsigned int"},
-        {'l', "long"},
-        {'m', "unsigned long"},
-        {'n', "nullptr_t"},
-        {'s', "short"},
-        {'t', "unsigned short"},
-        {'v', "void"},
-        {'w', "wchar_t"},
-        {'x', "long long"},
-        {'y', "unsigned long long"}
-    };
-    for (std::size_t i = 0; i < baseName.size(); i++) {
-        char c = baseName[i];
-        // std::cout << "[char] " << c << std::endl;
+utils::__detail::type_info
+utils::__detail::stringToType(const std::string& content, size_t i) {
+    type_info info;
+    //
+    while (i < content.size()) {
+        char c = content[i];
         switch (c) {
-            case 'P':
-                result.numPointers++;
+            case 'N': {
+                i++;
+                while (content[i] >= '0' && content[i] <= '9') {
+                    info.baseName.push_back(capture(content, i));
+                }
+                i++; // skip closing E
                 break;
+            }
+            case 'P':
+                info.isPointer = true;
+                info.templateTypes.push_back(stringToType(content, i+1));
+                return info;
             case 'K':
-                result.isConst = true;
+                info.isConst = true;
+                i++;
                 break;
             case 'V':
-                result.isVolatile = true;
-                break;
-            case 'A': {
+                info.isVolatile = true;
                 i++;
-                std::string buffer;
-                while (baseName[i] != '_') {
-                    buffer += baseName[i];
-                    i++;
-                }
-                result.arraySize = stoull(buffer);
                 break;
-            }
-            case 'I': {
-                std::size_t nestingLevel = 0;
-                std::string buffer;
+            case 'A':
                 i++;
-                while (baseName[i] != 'E' || nestingLevel > 0) {
-                    if (baseName[i] == 'I') {
-                        nestingLevel++;
-                    } else if (baseName[i] == 'E') {
-                        nestingLevel--;
-                    }
-                    buffer += baseName[i];
-                    i++;
-                }
-
-                auto types = typeStringToVector(buffer);
-                for (__detail::type_info& type : types) {
-                    result.templateTypes.push_back(std::move(type));
-                }
+                info.arraySize = catchNumber(content, i);
+                i++; // skip underline
                 break;
-            }
             default:
-                if (c >= 'a' && c <= 'z') {
-                    result.baseName = baseTypes[c];
-                } else if (c >= '0' && c <= '9') {
-                    std::string buffer;
-                    while (baseName[i] >= '0' && baseName[i] <= '9') {
-                        buffer += baseName[i];
-                        i++;
-                    }
-                    unsigned long long size = stoull(buffer);
-                    buffer.clear();
-                    for (unsigned long long j = 0; j < size; j++) {
-                        buffer += baseName[i];
-                        i++;
-                    }
-                    i--;
-                    result.baseName = buffer;
+                if (c >= '0' && c <= '9') {
+                    info.baseName.push_back(capture(content, i));
+                } else if (c >= 'a' && c <= 'z') {
+                    info.baseName.push_back(baseTypes.at(c));
+                    i++;
+                } else {
+                    // is this possible?
+                    assert(false);
                 }
         }
     }
-    return result;
+    //*/
+    return info;
 }
 
-std::string utils::__detail::infoToString(const utils::__detail::type_info& info) {
+std::string utils::__detail::typeToString(const utils::__detail::type_info& info) {
     std::string str;
+    if (info.isPointer) {
+        return typeToString(info.templateTypes[0]) + "*";
+    }
+
     if (info.isConst) str += "const ";
     if (info.isVolatile) str += "volatile ";
-    str += info.baseName;
+    for (size_t i = 0; i < info.baseName.size(); i++) {
+        if (i > 0) {
+            str += "::";
+        }
+        str += info.baseName[i];
+    }
     if (info.templateTypes.size() > 0) {
         str += "<";
-        for (std::size_t i = 0; i < info.templateTypes.size(); i++) {
+        for (size_t i = 0; i < info.templateTypes.size(); i++) {
             if (i > 0) {
                 str += ", ";
             }
-            str += infoToString(info.templateTypes[i]);
+            str += typeToString(info.templateTypes[i]);
         }
         str += ">";
-    }
-    for (std::size_t i = 0; i < info.numPointers; i++) {
-        str += "*";
     }
     if (info.arraySize > 0) {
         str += "[" + std::to_string(info.arraySize) + "]";
--- tests/type_tests.cpp
+++ tests/type_tests.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "macros.hpp"
 #include "type.hpp"
 
 template<typename T>
@@ -19,13 +20,94 @@ class C {};
 template<typename... T>
 class D {};
 
+template<typename T1, typename... T>
+class E {};
+
+class Test {};
+
+class BigClassName {};
+
+namespace example {
+	class Something {};
+
+	namespace inner {
+		class Hello {};
+	}
+}
+
+namespace {
+	class Scoped {};
+}
+
+template<typename T>
+void printType(T value) {
+	T var = value;
+	ECHO(typeid(var).name());
+}
+
+template<typename T>
+void printType() {
+	T var;
+	ECHO(typeid(var).name());
+}
+
 int main(int, char**) {
-    A<volatile int, B<int**, const int>> x[3];
-    std::cout << utils::type(x) << std::endl;
+    // A<volatile int, B<int**, const int>> x[3];
+    // std::cout << utils::type(x) << std::endl;
 
-    D<int**, const long double, volatile unsigned short, std::reference_wrapper<bool>, std::nullptr_t> y;
-    std::cout << utils::type(y) << std::endl;
+    // D<int**, const long double, volatile unsigned short, std::reference_wrapper<bool>, std::nullptr_t> y;
+    // std::cout << utils::type(y) << std::endl;
 
-    A<B<C,D<C,C,C,C>>, D<B<C,C>,A<C,D<>>>> z;
-    std::cout << utils::type(z) << std::endl;
+    // A<B<C,D<C,C,C,C>>, D<B<C,C>,A<C,D<>>>> z;
+    // std::cout << utils::type(z) << std::endl;
+
+	// int x = 1;
+	// example::Something y;
+	// char str[20];
+	// C c[20];
+
+	// printType<int>();
+	// printType<int&>(x);
+	// printType<int*>();
+	// printType<const int>(x);
+	// printType<const int*>(nullptr);
+	// printType<volatile int>();
+	// printType<const volatile int>(x);
+	// printType<const volatile int&>(x);
+	// printType<C>();
+	// printType<BigClassName>();
+	// printType<example::Something>();
+	// printType<Scoped>();
+	// printType<example::Something&>(y);
+	// BLANK
+	// printType<D<int&>>();
+	// printType<D<char,char,char>>();
+	// printType<A<char,char>>();
+	// printType<E<char,char,char>>();
+	// printType<example::inner::Hello>();
+	// // TRACE(utils::type(D<char,char,char>()));
+	// // TRACE(utils::type(E<char,char,char>()));
+	// printType<int* const * const>(nullptr);
+	// printType<decltype(str)>();
+	// printType<decltype(c)>();
+	// BLANK
+	// char (*argFn)(bool, int);
+	// char (*noArgFn)();
+	// void (*procedure)();
+	// auto lambdaArg = [](bool, int) { return 'a'; };
+	// auto lambdaNoArg = []() { return 'a'; };
+	// auto lambdaProcedure = [](){};
+	// printType<decltype(argFn)>();
+	// ECHO(typeid(*argFn).name());
+	// printType<decltype(noArgFn)>();
+	// printType<decltype(procedure)>();
+	// ECHO(typeid(lambdaArg).name());
+	// ECHO(typeid(lambdaNoArg).name());
+	// ECHO(typeid(lambdaProcedure).name());
+
+	// BLANK
+	// ECHO("----------------------------");
+	// BLANK
+
+	ECHO(utils::type(example::Something()));
 }
